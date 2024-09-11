757c1c458fbb6f1459cd6e3622efddfb8b1bdbff
nathancamolez-dev <nathan.camolez@unesp.br>
2024-09-11 14:28:34 -0300

    feat: All routes and configurations

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,25 @@
 import fastify from 'fastify'
-import { GoalsRoutes } from './http/routes/goals-routes'
+import { completionGoalsRoute } from './http/routes/completion-goal'
 import {
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
+import { createGoalRoute } from './http/routes/create-goal'
+import { pendingGoalsRoute } from './http/routes/pending-goals'
+import { getWeekSummaryRoute } from './http/routes/get-week-summary'
+import fastifyCors from '@fastify/cors'
 
 export const app = fastify()
+
+app.register(fastifyCors, {
+  origin: '*',
+})
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
-app.register(GoalsRoutes)
+app.register(createGoalRoute)
+app.register(pendingGoalsRoute)
+app.register(completionGoalsRoute)
+app.register(getWeekSummaryRoute)
